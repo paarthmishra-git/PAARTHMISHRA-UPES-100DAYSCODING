f27f20cbc2998fbf659ed39dfbde2e4db585ed70
Paarth Mishra <Paarth.37238@stu.upes.ac.in>
2026-09-19 13:34:09 +0530

    Add program to print asterisks based on group sizes

---
+++ day26q2.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    int groups[] = {1, 2, 5, 3, 1};
+    int count = sizeof(groups) / sizeof(groups[0]);
+
+    for (int i = 0; i < count; i++)
+    {
+        for (int j = 0; j < groups[i]; j++)
+        {
+            printf("*\n");
+        }
+
+        if (i < count - 1)
+        {
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
