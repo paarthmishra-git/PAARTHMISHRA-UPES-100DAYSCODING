82477d4a73649ba78d2664cd22b9e6f79190c3c1
Paarth Mishra <Paarth.37238@stu.upes.ac.in>
2026-09-26 23:05:30 +0530

    Add matrix rotation and print functionality
    Implement a function to rotate an N x N matrix 90 degrees clockwise and print the result.

---
+++ day40b.c
@@ -0,0 +1,46 @@
+//Rotate Matrix In-Place (90° clockwise)
+
+#include <stdio.h>
+
+#define N 3
+
+// Rotates an N x N matrix 90 degrees clockwise by transposing, then reversing each row
+void rotate(int matrix[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        int left = 0, right = N - 1;
+        while (left < right) {
+            int temp = matrix[i][left];
+            matrix[i][left] = matrix[i][right];
+            matrix[i][right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+
+void printMatrix(int matrix[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            printf("%d ", matrix[i][j]);
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[N][N] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    rotate(matrix);
+    printMatrix(matrix);
+    return 0;
+}
